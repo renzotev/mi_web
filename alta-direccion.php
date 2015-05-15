2ae9fb99b6741a5543d37e394321fc7e6babbaa2
renzotev <renzotelenta@gmail.com>
2015-05-14 23:57:36 -0500

    paginas

---
+++ alta-direccion.php
@@ -0,0 +1,18 @@
+<?php require_once 'parts/header.php'; ?>
+
+<div class="breadcrumb-wrapper hidden">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="index.php">Inicio</a></li>
+            <li class="active">Alta Dirección</li>
+        </ol>
+    </div>
+</div>
+
+<div class="general-content">
+    <div class="container">
+        <img src="img/puestos.png" class="aligncenter">
+    </div>
+</div>
+
+<?php require_once 'parts/footer.php'; ?>
---
+++ ministerio.php
@@ -0,0 +1,77 @@
+<?php require_once 'parts/header.php'; ?>
+
+<div class="breadcrumb-wrapper hidden">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="index.php">Inicio</a></li>
+            <li class="active">Ministerio</li>
+        </ol>
+    </div>
+</div>
+
+<div class="general-content">
+    <div class="container">
+        <h5>EL MINISTERIO</h5>
+        <br />
+        <h5>VISIÓN</h5>
+        <p>Ser la principal institución pública referente para la sociedad peruana en la promoción de la empleabilidad, el autoempleo y del trabajo decente, que garantice el respecto irrestricto y la vigencia de los derechos socio laborales y fundamentales en el ámbito laboral; así como, la consolidación del diáMlogo y la concertación socio laboral, para el desarrollo socio económico descentralizado con inclusión social.</p>
+        <br />
+        <h5>MISIÓN</h5>
+        <p>Somos el ente rector en materia de desarrollo y evaluación de las políticas socio laborales de trabajo y promoción de la empleabilidad e inserción laboral, el autoempleo y el trabajo decente a nivel nacional, garantizando el cumplimiento de la normativa laboral vigente, la prevención y solución de conflictos, la mejora de las condiciones de trabajo y el respeto de los derechos fundamentales del trabajador para el progreso de nuestras empresas en beneficio del desarrollo socioeconómico del país, en un marco democrático y de diálogo social.</p>
+
+        <ul class="list-buttons-1">
+            <li><a href="#">Marco Legal</a></li>
+            <li><a href="#">Organigrama</a></li>
+            <li><a href="#">R.O.F.</a></li>
+            <li><a href="#">M.O.</a></li>
+            <li><a href="#">T.U.P.A.</a></li>
+        </ul>
+
+
+        <h5>RESEÑA HISTÓRICA</h5>
+        <p>En 1901 el Estado interviene por primera vez en asuntos laborales a través del Ministerio de Gobierno y Policía, el cual asume la función de registrar los contratos del servicio doméstico.<br />
+        Doce años después, el 30 de enero de 1913, se expide un Decreto por el cual se encarga a la "Sección Obrera", la tarea de centralizar los datos sobre la situación y movimiento del trabajo.</p>
+
+        <p>El 30 de setiembre de 1919, en la Dirección de Fomento del Ministerio de Fomento se crea la "Sección del Trabajo". Posteriormente, con la dación del Decreto Supremo del 06 de marzo de 1920, se le encargan los servicios de Legislación del Trabajo, Estadística, Inspecciones, Asociaciones e Instituciones del Trabajo, Cultura y Previsión Social.</p>
+
+        <p>Un año después, el 12 de setiembre de 1921, se organiza la sección de "Asuntos Indígenas", dentro del Ministerio de Fomento, y el el 28 de abril de 1922, se crea el Concejo Superior de Trabajo y Previsión Social.</p>
+
+        <p>Cabe destacar la creación de la Inspección General del Trabajo el 02 de enero de 1929. Esta dependencia estuvo encargada de velar por el cumplimiento de leyes, decretos y resoluciones relativas al trabajo, en especial el de los niños y mujeres, tarea que antes estuvo reservada a las Municipalidades.</p>
+
+        <p>El 10 de enero de 1930, mediante Decreto Supremo, se aprobó el Reglamento Interno del Ministerio de Fomento y Obras Públicas, estableciéndose la Sección de Inspección General.</p>
+
+        <p>El 17 de junio de 1931, se promulgó el Decreto Ley Nº 7190 que declara que los fallos de la Sección de Trabajo y Previsión Social del Ministerio de Fomento, adquirían valor de cosa juzgada. A través del Decreto Ley Nº 7191, del mismo año, se autoriza al Ministerio de Fomento establecer Inspecciones Regionales de Trabajo en las zonas industriales en las que era necesario realizar inspecciones para atender conflictos colectivos. Así se crearon progresivamente las inspecciones regionales del trabajo de Talara, Cañete, La Libertad, Junín, Chancay, Callao, Arequipa, Lambayeque, Piura, Cuzco.</p>
+
+        <p>Con la dación de la Ley Nº 8124 del 05 de octubre de 1935, se crea el Ministerio de Salud Pública, Trabajo y Previsión Social, que incorporó a las Direcciones de Trabajo, de Previsión Social y de Asuntos Indígenas, pertenecientes al Ministerio de Fomento.<br />
+        En 1942, mediante Ley Nº 9679, expedida el 11 de diciembre de 1942, se dispuso que la Dirección de Trabajo y de Asuntos Indígenas del Ministerio de Salud Pública, Trabajo y Previsión Social, pasará a formar las direcciones del Ministerio de Justicia; el cual pasó a denominarse Ministerio de Justicia y Trabajo.</p>
+
+
+        <p><h6>Nace el Ministerio de Trabajo</h6></p>
+
+        <p>El 30 de abril de 1949, se inicia una nueva etapa en el campo del Trabajo: mediante Decreto Ley Nº 11009, se crea el Ministerio de Trabajo y Asuntos Indígenas, como una entidad independiente de otros Ministerios, acorde con la realidad histórica de aquellos tiempos.</p>
+
+        <p>El ministerio fue instalado el 27 de octubre de ese mismo año, al conmemorarse el Primer Aniversario del Movimiento Restaurador de Arequipa.<br />
+        El 31 de diciembre de 1965, mediante la Ley Nº 15850, Ley Anual de Presupuesto Funcional de la República para 1966, se varía el nombre de nuestro portafolio, denominándolo Ministerio de Trabajo y Comunidades.<br />
+        Con fecha 3 de diciembre de 1968, a través del Decreto Ley Nº 17271, Ley de Ministerios, nuevamente se modifica el nombre por Ministerio de Trabajo. Además, a través del Decreto Ley Nº 17524 del 21 de marzo de 1969, se le otorga una nueva estructura y funciones.</p>
+
+        <p>En 1971, mediante Decreto Ley Nº 19040, se declara en reorganización el Ministerio de Trabajo. Además a través de la Ley Nº 23212 del 24 de julio de 1980, se adecuan sus estructuras y las funciones de los órganos, adicionando a la tarea de regulación de las normas laborales, las normas de higiene y seguridad y la coordinación de la formación profesional, manteniendo relaciones con organismos internacionales, regionales y sub regionales, especializados en el área socio laboral.</p>
+
+        <p>Con la dación del Decreto Legislativo Nº 140, con fecha 12 de junio de 1981, se expide la Ley de Organización de los sectores Trabajo y Promoción Social. Se concibe al primer sector como el rector de la administración pública del trabajo, y al segundo como el encargado de fomentar y dirigir la política nacional de empleo.</p>
+
+        <p>El Decreto Legislativo Nº 568, con fecha 4 de abril de 1990, se promulgó la Ley de Organización y Funcionamiento de nuestro Ministerio. Dicha norma fue reglamentada en lo que respecta a su organización y funciones, a través del Decreto Supremo Nº 038-90 -TR de fecha 08 de junio de 1990.<br />
+        La Ley Orgánica del Ministerio de Trabajo y Promoción Social fue aprobada mediante Decreto Ley Nº 25927 del 4 de diciembre de 1992 y reglamentada por Resolución Ministerial Nº 012-93-TR, a través del cual se determinó la competencia, atribuciones, estructura y funciones del Portafolio. Hay que señalar que en esta norma se registra la labor que cumple el Consejo Nacional de Trabajo y Promoción Social, como órgano consultivo.</p>
+
+        <p>El 18 de abril del 2001 se expide el Decreto Supremo Nº 009-2001-TR por el cual se dispone la reestructuración organizativa de nuestra institución en función a la evaluación de sus competencias y responsabilidades, con el objeto de mejorar su eficiencia, racionalizar sus gastos y generar ahorro público. Todo ello con el propósito de adecuarnos a los cambios en la normatividad actual.</p>
+
+        <p><h6>Nuestro local institucional</h6></p>
+
+        <p>Nuestra sede central de la Av. Salaverry se construyó en un terreno de 9,451.85 metros cuadrados de propiedad del Estado, adquirido en el año 1870. Cabe señalar que formó parte del antiguo predio rústico Santa Beatriz. </p>
+
+        <p>El área construida es de 36,799 metros cuadrados y consta de 12 pisos. La edificación se inició el 27 de octubre de 1952, siendo Presidente Constitucional de la República el General de División don Manuel A. Odría y Ministro de Trabajo y Asuntos Indígenas el General de Brigada don Armando Artola de Pozo.<br />
+        El arquitecto encargado de la obra fue don Alejandro Garland Roel y la inauguración se realizó el 30 de abril de 1955, siendo Ministro de Trabajo y Asuntos Indígenas Víctor A. Casagrandi.</p>
+        <br />
+        <img src="img/mis-vis.jpg" class="aligncenter">
+    </div>
+</div>
+
+<?php require_once 'parts/footer.php'; ?>
